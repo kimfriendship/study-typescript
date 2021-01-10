3e17104a9de3b215cf18c0efc8f26f85e2f0a03f
kimfriendship <kfriendship94@gmail.com>
2021-01-10 21:47:26 +0900

    :sparkles: Learn basic types

---
+++ src/chap01_basictype.ts
@@ -0,0 +1,33 @@
+let numVal: number;
+let strVal: string;
+let boolVal: boolean;
+let undefinedVal: undefined;
+let nullVal: null;
+let symVal: symbol;
+let objVal: object;
+
+numVal = 2;
+numVal = 9.4334;
+strVal = 'hi';
+strVal = String('hey');
+boolVal = false;
+undefinedVal = null;
+nullVal = undefined;
+symVal = null;
+symVal = Symbol();
+objVal = undefined;
+objVal = {};
+objVal = {key: 'value'};
+objVal = new String('type');
+
+let nameList: string[];
+let anyList: any[];
+let user: {name: string, score: number};
+let tuple: [number, string, any];
+
+nameList = ['my', 'name', 'is', 'kimfriendship'];
+anyList = [numVal, strVal, boolVal, undefinedVal, nullVal, symVal];
+user = {name: 'woojung', score: 90};
+tuple = [1003, 'study', null];
+
+
